0b58745bc85918739b689abfda047ecb2adaabdf
xupremix <xupremix.me@gmail.com>
2026-07-23 03:35:46 +0200

    fix tests + check cuda tests

--- crates/kindle-core/tests/compile_fail/forward_broadcast_mismatch.rs
+++ crates/kindle-core/tests/compile_fail/forward_broadcast_mismatch.rs
@@ -1,11 +1,10 @@
-use kindle_core::prelude::*;
 use kindle_core::prelude::dummy::DummyBackend;
-use typenum::{U1, U2, U3};
+use kindle_core::prelude::*;
 
 fn main() {
     let t1: Tensor<(typenum::U1, typenum::U2), DummyBackend<f32, Cpu>> = Tensor::zeros(()).unwrap();
     let t2: Tensor<(typenum::U3, typenum::U3), DummyBackend<f32, Cpu>> = Tensor::zeros(()).unwrap();
-    
+
     // Broadcast of (1, 2) and (3, 3) should fail since 2 != 3 and neither is 1!
     let _out = t1.broadcast_add(&t2).unwrap();
 }
